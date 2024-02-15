3545083effd95bad7e927efa5189339d8eea6917
Areya <Areya.EmmanuelSingh26@ycdsbk12.ca>
2024-02-14 20:46:32 -0500

    Completed Power Document

--- Power.java
+++ Power.java
@@ -1,13 +1,26 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
+  * This program lets you enter a base and an exponent and will give you the answer
   * @author:
   */
   
   public void run() {
     
-    // start coding here
+    // Declare Variables
+    double dblA;
+    double dblB;
+    double dblFinalNumber;
+
+    // Input from Users
+    dblA = readInt("Enter the base of the exponent: ");
+    dblB = readInt("Enter the exponent: ");
+
+    // Calulations 
+    dblFinalNumber = Math.pow(dblA, dblB);
+
+    // Output
+    System.out.println(dblA + " to the power of " + dblB + " is " + dblFinalNumber);
     
   }
 }
