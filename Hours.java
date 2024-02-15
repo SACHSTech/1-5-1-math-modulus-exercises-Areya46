899ce6ab08b18baf654dab0ee64c1b2aca6dd676
Areya <Areya.EmmanuelSingh26@ycdsbk12.ca>
2024-02-15 12:44:46 -0500

    Completed Hours Program

--- Hours.java
+++ Hours.java
@@ -1,13 +1,26 @@
 class Hours extends ConsoleProgram {
 
   /**
-  * This code lets the user enter a degree measure in Fahrenheit, and that prints the result in celsius degree measure.
+  * This program lets you enter a number of hours, and  converts it to day and hours.
   * @author: A. Emmanuel-Singh
   */
   
   public void run() {
     
-    // start coding here
-    
+    // Declare variables
+    int intHours;
+    int intDays;
+    int intMod;
+
+    // Input form users
+    intHours = readInt("Enter the number of hours you would like to convert to days: ");
+
+    // Calculations 
+    intDays = (intHours / 24);
+    intMod = (intHours % 24);
+
+    // Output
+    System.out.println( intHours + " hours is " + intDays + " and " + intMod + " hours."); 
+
   }
 }
