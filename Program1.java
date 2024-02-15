0d08c8cc8c79d5696a663d3dc5af823fc4ef50ea
Areya <Areya.EmmanuelSingh26@ycdsbk12.ca>
2024-02-15 13:07:43 -0500

    Completed Program1 (Km to Miles)

--- Program1.java
+++ Program1.java
@@ -1,13 +1,23 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * This program les you enter a distance in kilometers and that covertsit into miles
+  * @author: A. Emmanuel-Singh
   */
   
   public void run() {
-    // your code goes here
-    
+    // Declare variables 
+    double dblKm;
+    double dblMiles;
+
+    // Input from user 
+    dblKm = readDouble("Please enter the distance (in Kilomiters) that you would like to convert into miles: ");
+
+    // Calculations                                                                                                     
+    dblMiles = dblKm * 0.621371;
+
+    // Output
+    System.out.println(dblKm + " in Miles is " + dblMiles);
     
   }
 }
